8d90c1fce30c77e641221ac7989951f7bc18a8a8
Sidney Sadel <tuj60668@temple.edu>
2022-04-06 10:35:25 -0400

    Initial Commit

---
+++ Vanguard DEMO/Vanguard_DEMOApp.swift
@@ -0,0 +1,17 @@
+//
+//  Vanguard_DEMOApp.swift
+//  Vanguard DEMO
+//
+//  Created by Sidney Sadel on 4/6/22.
+//
+
+import SwiftUI
+
+@main
+struct Vanguard_DEMOApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
